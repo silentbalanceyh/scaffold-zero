23ad2a3c7e4287cc1ea97a0b6e9c8bb05ac07e1b
emma19910916 <1096721729@qq.com>
2023-06-28 11:42:54 +0800

    修改build.bat有一行命令错误

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 mvnd clean package install -DskipTests=true -Dmaven.javadoc.skip=true -Dmaven.source.skip=true -Dmaven.compile.fork=true -T 1C
 copy zo-inst\zo-dpma\target\inst-load.jar zo-app\app@jar\
-copy zo-inst\zo-dpma\target\inst-menu.jar zo-app\app@jar\
+copy zo-inst\zo-menu\target\inst-menu.jar zo-app\app@jar\
