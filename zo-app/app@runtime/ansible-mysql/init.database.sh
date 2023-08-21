df4245ec56957144789ea98079cd1e7c2f530ecd
emma19910916 <1096721729@qq.com>
2023-08-21 15:14:16 +0800

    撤销本地变动

--- zo-app/app@runtime/ansible-mysql/init.database.sh
+++ zo-app/app@runtime/ansible-mysql/init.database.sh
@@ -12,8 +12,8 @@ fi
 
 # MySQL 账号设置
 # 1. Account
-/usr/local/mysql/bin/mysql -uzero -p"$DB_PASSWORD" -h"$DB_HOST" < "$DB_RUN"/init.account.sql
+mysql -uzero -p"$DB_PASSWORD" -h"$DB_HOST" < "$DB_RUN"/init.account.sql
 # 2. Database
-/usr/local/mysql/bin/mysql -uzero -p"$DB_PASSWORD" -h"$DB_HOST" < "$DB_RUN"/init.app-database.sql
+mysql -uzero -p"$DB_PASSWORD" -h"$DB_HOST" < "$DB_RUN"/init.app-database.sql
 # 3. Database Workflow
-/usr/local/mysql/bin/mysql -uzero -p"$DB_PASSWORD" -h"$DB_HOST" < "$DB_RUN"/init.workflow-engine.sql
+mysql -uzero -p"$DB_PASSWORD" -h"$DB_HOST" < "$DB_RUN"/init.workflow-engine.sql
